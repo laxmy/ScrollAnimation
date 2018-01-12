f95fcf1d729772d0f634d6abcfbedef0b7a71c9b
laxmy <laxmysubu@gmail.com>
2018-01-12 10:39:37 -0600

    Scroll Effect Landing Page

---
+++ js/scrollEffect.js
@@ -0,0 +1,27 @@
+window.sr = ScrollReveal();
+sr.reveal('.navbar',{
+  duration:2000,
+  origin:'bottom'
+});
+sr.reveal('.showcase-img-left',{
+  duration:2500,
+  origin:'top',
+  distance: '300px'
+});
+sr.reveal('#product-block1',{
+  duration:2500,
+  origin:'left',
+  distance: '100px',
+  viewFactor: 0.3
+});
+sr.reveal('#product-block2',{
+  duration:2500,
+  origin:'right',
+  distance: '100px',
+  viewFactor: 0.3
+});
+sr.reveal('.content',{
+  duration:2000,
+  origin:'bottom',
+  viewFactor: 0.3
+});
